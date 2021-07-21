e8d63228f5201d98f6da8907a9349498cf4fb4cc
Eugene Kravec <krava255@gmail.com>
2021-07-21 17:27:16 +0300

    go

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const fruits = ["apple", "peach", "pear", "banana"];
+
+// Change code below this line
+const lastElementIndex = fruits.length-1;
+console.log(lastElementIndex);
+
+const lastElement = fruits[fruits.length-1];
+console.log(lastElement);
