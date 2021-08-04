25647f1c28129ba8e178148be81e76a470c0f7d4
Eugene Kravec <krava255@gmail.com>
2021-08-04 16:24:17 +0200

    Destructuring done

--- js/destructuring.js
+++ js/destructuring.js
@@ -12,7 +12,10 @@ function showTitle(name) {
 
 showTitle('Parfume');
 console.log(product.title);
+
 showTitle(product.title);
+const { title: myTitle } = product; // деструктуризация
+showTitle(myTitle);
 
 const products = {
   parfums: {
@@ -37,5 +40,30 @@ console.log(products.parfums.female[0].title);
 let title = products.parfums.female[0].title;
 console.log(title);
 
-const { title: productTitle } = products.parfums.female[0];
-console.log('pT', productTitle);
+// Destructuring
+// const { property1 } = obj
+
+const { title: productTitle, name, price } = products.parfums.female[0];
+console.log('Rename productTitle', productTitle);
+console.log('name', name);
+console.log('price', price);
+
+const { title: productTitleMale, name: nameMale, price: priceMale } = products.parfums.male[0];
+console.log('Rename productTitleMale', productTitleMale);
+console.log('name', nameMale);
+console.log('price', priceMale);
+
+const { female, male } = products.parfums;
+console.log(female, male);
+
+// Деструктуризация массивов
+
+const rgb = [255, 39, 45];
+console.log(rgb);
+
+// const [] = array
+
+const [red, , blue] = rgb;
+console.log('red:', red);
+// console.log('green:', green);
+console.log('blue:', blue);
