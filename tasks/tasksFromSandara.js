a7a9f4e6d9f7c3ac7f510863f1a4b31fc3df695e
Eugene Kravec <krava255@gmail.com>
2021-08-17 16:19:43 +0300

    Some task

--- tasks/tasksFromSandara.js
+++ tasks/tasksFromSandara.js
@@ -66,3 +66,34 @@ console.log(atTheOldToad.addPotion({ name: "Stone skin2", price: 240 }));
 console.log(atTheOldToad.removePotion("Dragon breath"));
 console.log(atTheOldToad.potions);
 console.log(atTheOldToad.updatePotionName("Stone skin", "Invulnerability potion"));
+
+const pizzaPalace = {
+    pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+    order(pizzaName, onSuccess, onError) {
+        if (this.pizzas.includes(pizzaName)) {
+            return onSuccess(pizzaName)
+        }
+        return onError(`В ассортименте нет пиццы с названием ${pizzaName}.`)
+    }
+}
+
+
+// Пиши код выше этой строки
+
+// Колбэк для onSuccess
+function makePizza(pizzaName) {
+    return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+}
+
+// Колбэк для onError
+function onOrderError(error) {
+    return `Ошибка! ${error}`;
+}
+
+// Вызовы метода с колбэками
+
+pizzaPalace.order('Четыре нарезона', makePizza, onOrderError);
+pizzaPalace.order('Биг майк', makePizza, onOrderError);
+pizzaPalace.order('Венская', makePizza, onOrderError);
+
+console.log(pizzaPalace.order('Аль Копчино', makePizza, onOrderError));
