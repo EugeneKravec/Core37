0eea2d69544ee1b1c1137ba53cea64a69efd38da
Eugene Kravec <krava255@gmail.com>
2021-08-20 23:11:34 +0300

    Did methods of Arrays

--- js/methodsOfArr.js
+++ js/methodsOfArr.js
@@ -41,10 +41,80 @@ const numbers = [1, 2, 3, 4, 5]
 const forEachResults = numbers.forEach((num) => {
     // console.log(num);
 })
-console.log(forEachResults);
+console.log('forEachResults', forEachResults);
+
+// ========================================
 
 const mapResults = numbers.map((item) => {
     console.log(item);
-    return item
+    return item + 1
 })
-console.log(mapResults);
+console.log('Исходный:', numbers);
+console.log('mapResults', mapResults);
+
+// =========================================
+
+const filterResult = numbers.filter((elem) => {
+    return elem >= 2 && elem <= 4
+})
+console.log('filterResult', filterResult);
+
+// =======================================
+
+const findResult = numbers.find((i) => {
+    return i > 2 && i < 5
+})
+console.log('findResult', findResult);
+// Filter вернет только 1 элемент, прошедший условие
+// ========================================
+
+const everyResult = numbers.every((elem) => {
+    // console.log('everyResult', elem);
+    return elem > 3
+})
+console.log('everyResult', everyResult);
+
+// ========================================
+
+const someResult = numbers.some((elem) => {
+    // console.log('someResult', elem);
+    return elem > 3
+})
+console.log('someResult', someResult);
+
+// ========================================
+
+const fruit = ['banana', 'apples', 'orange']
+const sortResult = fruit.sort().reverse()
+console.log('sortResult', sortResult);
+console.log(fruit);
+
+const ages = [54, 32, 17, 85, 100, -22, -3]
+const sortAges = ages.sort((a, b) => {
+    return a - b
+})
+console.log(sortAges);
+// ========================================
+
+const reduceResultArray = numbers.reduce((acc, elem) => {
+    // console.log(acc, elem);
+    acc.push(elem)
+    return acc
+}, [])
+console.log('reduceResultArray', reduceResultArray);
+
+const reduceResultObj = numbers.reduce((newObj, number) => {
+    // console.log(newObj, number);
+    newObj[number] = number
+    return newObj
+}, {})
+console.log('reduceResultObj', reduceResultObj);
+
+const reduceResultnumber = numbers.reduce((total, num) => {
+    // console.log(total, num);
+    total += num
+    return total
+}, 0)
+console.log('reduceResultnumber', reduceResultnumber);
+
+// ========================================
